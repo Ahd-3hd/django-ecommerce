66899c0a933b63c866452ffc2c2ef89a185e7feb
ahd <ahdhani0@gmail.com>
2021-03-07 22:09:47 +0300

    create templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Category, Product
+
+#TODO convert to class based view later
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html',{'products':products})
