3c6cf72b9f7e0e0eebf699fb8b3a7d321111c1b7
ahd <ahdhani0@gmail.com>
2021-03-07 22:56:45 +0300

    dynamic url linking

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Category(models.Model):
     #https://en.wikipedia.org/wiki/Database_index
@@ -10,6 +11,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
     
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
     # to reference data by name
     def __str__(self):
         return self.name
@@ -37,5 +41,8 @@ class Product(models.Model):
         verbose_name_plural = 'Products'
         # the - is for descending order
         ordering = ('-created',)
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
     def __str__(self):
         return self.title
