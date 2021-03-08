be7553962680c869b014ed802d9d0e5ebc55c8bf
ahd <ahdhani0@gmail.com>
2021-03-08 05:54:19 +0300

    create the static folder and include it

--- core/settings.py
+++ core/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 # url to media/images
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
